516b73aedb9ce627d8cb60626fcd7f7528cfd62f
obijuan <obijuan@f3278bb9-4a81-0410-9110-86323399464e>
2010-03-08 15:20:27 +0000

    -> Added example Test-servocontroller-1
    git-svn-id: http://svn.iearobotics.com/openmr/trunk@3 f3278bb9-4a81-0410-9110-86323399464e

--- examples/Test-servocontroller-1.cpp
+++ examples/Test-servocontroller-1.cpp
@@ -31,7 +31,7 @@ int main(int argc, char ** argv)
 
    if (argc==1)
      //-- Default file
-     envfile="./Minicube-I.env.xml";
+     envfile="./models/Minicube-I.env.xml";
    else
      envfile = argv[1];
 
