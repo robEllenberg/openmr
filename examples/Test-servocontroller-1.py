b23e877a22b4a005286e8b22b9c021c2db932df1
obijuan <obijuan@f3278bb9-4a81-0410-9110-86323399464e>
2010-03-08 15:37:37 +0000

    -> Add the Test-servocontroller-1.py example
    git-svn-id: http://svn.iearobotics.com/openmr/trunk@4 f3278bb9-4a81-0410-9110-86323399464e

---
+++ examples/Test-servocontroller-1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+#// This program is free software: you can redistribute it and/or modify
+#// it under the terms of the GNU Lesser General Public License as published by
+#// the Free Software Foundation, either version 3 of the License, or
+#// at your option) any later version.
+#//
+#// This program is distributed in the hope that it will be useful,
+#// but WITHOUT ANY WARRANTY; without even the implied warranty of
+#// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#// GNU Lesser General Public License for more details.
+#//
+#// You should have received a copy of the GNU Lesser General Public License
+#// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import with_statement # for python 2.5
+__author__ = 'Juan Gonzalez'
+__copyright__ = 'Copyright (C) 2010 Juan Gonzalez (juan@iearobotics.com)'
+__license__ = 'GPLv3 license'
+
+#-- This is an example for testing the servocontroller located in the
+#-- Modular Robot Open Rave plugin
+#-- The two servos of the Minicube-I modular robot are both set to 45
+#-- and -45 degrees.
+#-- The Minicube-I modular robot is composed of two Y1 modules.
+
+
+from openravepy import *
+from numpy import *
+import time
+import sys
+
+def run():
+
+    #-- Read the name of the xml fiel passed as an argument
+    #-- or use the default name
+    try:
+        file_env = sys.argv[1]
+    except IndexError:
+        file_env = 'models/Minicube-I.env.xml'
+
+    env = Environment()
+    env.Load(file_env)
+    env.SetViewer('qtcoin')
+
+    with env:
+        robot = env.GetRobots()[0]
+        robot.SetController(env.CreateController('servocontroller'))
+        env.StopSimulation()
+        env.StartSimulation(timestep=0.001)
+
+    starttime = time.time()
+    while True:
+        #-- Set the servos position to 45 degrees
+        ref_pos = [45 for j in robot.GetJoints()]
+        robot.GetController().SendCommand('setpos '+' '.join(str(f) for f in ref_pos))
+        time.sleep(1.0)
+
+	#-- Set the servos position to -45 degrees
+	ref_pos = [-45 for j in robot.GetJoints()]
+        robot.GetController().SendCommand('setpos '+' '.join(str(f) for f in ref_pos))
+        time.sleep(1.0)
+
+if __name__=='__main__':
+    run()
