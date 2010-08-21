3ed7ba42f7b2a85c645632a1dfeee41758a950b1
obijuan <obijuan@f3278bb9-4a81-0410-9110-86323399464e>
2010-08-21 14:07:51 +0000

    -> Minicube-I.cpp example is working with rev. 1715
    git-svn-id: http://svn.iearobotics.com/openmr/trunk@70 f3278bb9-4a81-0410-9110-86323399464e

--- examples/Minicube-I-1.cpp
+++ examples/Minicube-I-1.cpp
@@ -11,9 +11,11 @@ using namespace OpenRAVE;
 using namespace std;
 
 
+ViewerBasePtr viewer;
+
 void SetViewer(EnvironmentBasePtr penv, const string& viewername)
 {
-    RaveViewerBasePtr viewer = penv->CreateViewer(viewername);
+    viewer = penv->CreateViewer(viewername);
     BOOST_ASSERT(!!viewer);
 
     // attach it to the environment:
@@ -31,31 +33,27 @@ int main(int argc, char ** argv)
 
    if (argc==1)
      //-- Default file
-     envfile="./models/Minicube-I.env.xml";
+     envfile="models/Minicube-I.env.xml";
    else
      envfile = argv[1];
 
     // create the main environment
     EnvironmentBasePtr penv = CreateEnvironment(true);
     penv->StopSimulation();
-
+    penv->SetDebugLevel(Level_Debug);
 
     boost::thread thviewer(boost::bind(SetViewer,penv,"qtcoin"));
-    {
-        // lock the environment to prevent changes
-        EnvironmentMutex::scoped_lock lock(penv->GetMutex());
-
-        // load the scene
-        penv->Load(envfile);
+    // load the scene
+    if( !penv->Load(envfile) ) {
+        penv->Destroy();
+        return 2;
     }
 
-    //-- Set the transform matrix for the camera view
-    RaveTransformMatrix<float> M;
-    RaveVector<float> rotquad(0.505073, 0.268078, 0.395983, 0.718493);
-    RaveVector<float> trans(0.412915, 0.156822, 0.285362);
-    M.trans = trans;
-    //M.rotfromquat (rotquad);
-    RaveTransform<float> Tcamera(M);
+    //-- Set the transform for the camera view
+    RaveVector<float> rotation(0.426572, 0.285257, 0.469795, 0.718301);
+    RaveVector<float> translation(0.589395, 0.0782605, 0.262882);
+    RaveTransform<float> T(rotation,translation);
+    viewer->SetCamera(T);
 
     //-- Get the robot
     std::vector<RobotBasePtr> robots;
@@ -70,19 +68,19 @@ int main(int argc, char ** argv)
     probot->SetController(pcontroller,"");
 
     stringstream os,is;
-    is << "setamplitude 45 45 ";
+    is << "setamplitude 50 50 ";
     pcontroller->SendCommand(os,is);
 
-    is << "setinitialphase 0 120 ";
+    is << "setinitialphase 0 -120 ";
     pcontroller->SendCommand(os,is);
 
     is << "setoffset 0 0 ";
     pcontroller->SendCommand(os,is);
 
-    is << "setperiod 2 ";
+    is << "setperiod 1.5 ";
     pcontroller->SendCommand(os,is);
 
-    const dReal STEP = 0.001;
+    const dReal STEP = 0.005;
     penv->StartSimulation(STEP);
     usleep(1000);
 
